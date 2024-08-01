9191bd6c7305ba7f092c066607ac6ce4eb7872b6
Similoluwa10 <127258936+Similoluwa10@users.noreply.github.com>
2024-08-01 18:02:24 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-$(document).ready( function() {
+$(document).ready(function () {
     $('.dropdown-toggle').dropdown();
-    });
+});
 
+
+
+
+function onSignUp(){
+    document.getElementById('approval-section').style.display = 'block'
+    document.getElementById('sign-up-section').style.display = 'none'
+}
